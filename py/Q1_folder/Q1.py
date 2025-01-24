bc85378cb06e2a952d0725e6d193e262427a058b
Michael Fetissov-Carden <up2065105@myport.ac.uk>
2025-01-24 12:46:25 +0000

    Update Q1.py
    Cleaning the data for relevant columns only.

--- py/Q1_folder/Q1.py
+++ py/Q1_folder/Q1.py
@@ -1,1 +1,4 @@
-
+# Selecting relevant columns and handling missing values
+satellite_data = load_data()
+columns_required = ["Class of Orbit", "Type of Orbit", "Perigee (Kilometers)", "Apogee (Kilometers)", "Inclination (Degrees)"]
+processed_data = satellite_data[columns_required].copy()
