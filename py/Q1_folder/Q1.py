fae817b275ae4f99ccf3dac99a3e28b2bee05e6b
Michael Fetissov-Carden <up2065105@myport.ac.uk>
2025-01-25 13:49:13 +0000

    renamed:    Q1.ipynb -> py/Q1_folder/Q1.ipynb 	deleted:    py/Q1_folder/Q1.py

--- py/Q1_folder/Q1.py
+++
@@ -1,4 +0,0 @@
-# Selecting relevant columns and handling missing values
-satellite_data = load_data()
-columns_required = ["Class of Orbit", "Type of Orbit", "Perigee (Kilometers)", "Apogee (Kilometers)", "Inclination (Degrees)"]
-processed_data = satellite_data[columns_required].copy()
